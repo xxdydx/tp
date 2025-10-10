d4d234e4b67e3f0054cec411f9e8b297737fb62a
dylan <dylan.ananda@gmail.com>
2025-10-10 15:39:03 +0800

    Fix import ordering

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
 import seedu.address.testutil.AddressBookBuilder;
 
 /**
