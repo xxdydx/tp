0e60d6f20957d4558c4508928954b5b4e0ba8ee7
Arul <official@arul.me>
2025-10-23 00:22:32 +0800

    Remove unused import to fix checkstyle

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -11,7 +11,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_WEDDING_DATE_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_WEDDING_DATE_BOB;
 
 import java.util.ArrayList;
 import java.util.Arrays;
