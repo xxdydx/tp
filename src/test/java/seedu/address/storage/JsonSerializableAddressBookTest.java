a174ec74c5f193e9164fd09b48db57fecc070c90
Arul <official@arul.me>
2025-10-26 13:41:26 +0800

    checkstyle fix

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -19,7 +19,8 @@ public class JsonSerializableAddressBookTest {
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
-    private static final Path SAME_NAME_DIFFERENT_PHONE_FILE = TEST_DATA_FOLDER.resolve("sameNameDifferentPhoneAddressBook.json");
+    private static final Path SAME_NAME_DIFFERENT_PHONE_FILE =
+            TEST_DATA_FOLDER.resolve("sameNameDifferentPhoneAddressBook.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
