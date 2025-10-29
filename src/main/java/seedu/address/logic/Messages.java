7153519187c3f59d16f8e6b3df27408cb907f3a2
Javier Lim <javierlimt6@gmail.com>
2025-10-29 12:05:33 +0800

    fix wrong tag format

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -17,7 +17,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
-                "Multiple values specified for the following single-valued field(s): ";
+            "Multiple values specified for the following single-valued field(s): ";
 
     /**
      * Returns an error message indicating the duplicate prefixes.
@@ -61,7 +61,10 @@ public class Messages {
                 builder.append("; Partner: ").append(partner));
 
         builder.append("; Tags: ");
-        person.getTags().forEach(builder::append);
+        String tags = person.getTags().stream()
+                .map(tag -> tag.tagName)
+                .collect(Collectors.joining(", "));
+        builder.append(tags);
         return builder.toString();
     }
 
