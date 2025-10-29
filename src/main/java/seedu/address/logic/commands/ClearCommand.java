fe3ab3a8cca00687b38597347899811e369dde59
Clement <clementaditya@gmail.com>
2025-10-29 12:07:59 +0800

    Add different response when clear is used on a cleared address book

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -14,11 +14,18 @@ public class ClearCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears all entries from the address book.\n"
             + "Example: " + COMMAND_WORD;
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_ALREADY_EMPTY = "Address book is already empty.";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+
+        // Check if address book is already empty
+        if (model.getAddressBook().getPersonList().isEmpty()) {
+            return new CommandResult(MESSAGE_ALREADY_EMPTY);
+        }
+
         model.setAddressBook(new AddressBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
