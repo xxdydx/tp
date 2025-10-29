5a6fa0bfe092f29fa6a8397f71fbdf761747e103
celine <josephinecelinetj@gmail.com>
2025-10-29 22:48:42 +0800

    Remove unused imports

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -12,7 +12,6 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Partner;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.PersonType;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Price;
 import seedu.address.model.tag.Tag;
